564fba7ae115b5eca592f67d5f66b8cd6cf08075
Martin Wimpress <code@flexion.org>
2016-08-26 10:47:50 +0100

    Initial commit of v0.4

---
+++ config.js
@@ -0,0 +1,20 @@
+// the name of your app as used in the click package (required)
+var webappName = "msainsburyscouk-owo.flexiondotorg"
+
+// the start url of your app (required)
+var webappUrl = "http://m.sainsburys.co.uk"
+
+// the the pattern that defines which links are considered
+// local and which are opened in an external browser (required)
+var webappUrlPattern = "https?://*.sainsburys.co.uk/*,https?://*.sainsburys-live-well-for-less.co.uk/*,https?://*.sainsburysebooks.co.uk/*,https?://*.sainsburysentertainment.co.uk/*,https?://*.phoneshopsainsburys.co.uk/*,https?://*.sainsburysbank.co.uk/* http://m.sainsburys.co.uk"
+
+// a user agent override (optional)
+// var webappUA = "uncomment this line and set your User Agent string here between these quotes, if you need to override it"
+
+// Haptic feedback for links (note, this does not work if your site
+// uses javascript functions to open links) (optional)
+// var hapticLinks = "true"
+
+// Audible feedback when clicking links (the same constraints as
+// for hapticLinks apply) (optional)
+// var audibleLinks = "true"
