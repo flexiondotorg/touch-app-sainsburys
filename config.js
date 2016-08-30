6b8c7bb9c83c2fff739b98a97a4a82adf0bb9b1e
Martin Wimpress <code@flexion.org>
2016-08-30 16:34:45 +0100

    Add support for more Sainsburys domains linked within the webapp.

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 var webappName = "msainsburyscouk-owo.flexiondotorg"
 
 // the start url of your app (required)
-var webappUrl = "http://m.sainsburys.co.uk"
+var webappUrl = "https://m.sainsburys.co.uk"
 
 // the the pattern that defines which links are considered
 // local and which are opened in an external browser (required)
-var webappUrlPattern = "https?://*.sainsburys.co.uk/*,https?://*.sainsburys-live-well-for-less.co.uk/*,https?://*.sainsburysebooks.co.uk/*,https?://*.sainsburysentertainment.co.uk/*,https?://*.phoneshopsainsburys.co.uk/*,https?://*.sainsburysbank.co.uk/* https://m.sainsburys.co.uk"
+var webappUrlPattern = "https?://*.sainsburys.co.uk/*,https?://*.sainsburys-live-well-for-less.co.uk/*,https?://*.sainsburysebooks.co.uk/*,https?://*.sainsburysentertainment.co.uk/*,https?://*.phoneshopsainsburys.co.uk/*,https?://*.sainsburysbank.co.uk/*,https?://*.homemadebyyou.co.uk/*,https?://*.sainsburysenergy.com/*,https?://*.sainsburyshome.co.uk/*,https?://*.sainsburyslivingarchive.co.uk/*,https?://*.sainsburysgifts.co.uk/* https://m.sainsburys.co.uk"
 
 // a user agent override (optional)
 // var webappUA = "uncomment this line and set your User Agent string here between these quotes, if you need to override it"
