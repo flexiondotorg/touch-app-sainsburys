49a1428cc699872be6e386b8bb357f42f0e1e2a4
Martin Wimpress <code@flexion.org>
2016-08-30 15:57:25 +0100

    Use https by default.

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var webappUrl = "http://m.sainsburys.co.uk"
 
 // the the pattern that defines which links are considered
 // local and which are opened in an external browser (required)
-var webappUrlPattern = "https?://*.sainsburys.co.uk/*,https?://*.sainsburys-live-well-for-less.co.uk/*,https?://*.sainsburysebooks.co.uk/*,https?://*.sainsburysentertainment.co.uk/*,https?://*.phoneshopsainsburys.co.uk/*,https?://*.sainsburysbank.co.uk/* http://m.sainsburys.co.uk"
+var webappUrlPattern = "https?://*.sainsburys.co.uk/*,https?://*.sainsburys-live-well-for-less.co.uk/*,https?://*.sainsburysebooks.co.uk/*,https?://*.sainsburysentertainment.co.uk/*,https?://*.phoneshopsainsburys.co.uk/*,https?://*.sainsburysbank.co.uk/* https://m.sainsburys.co.uk"
 
 // a user agent override (optional)
 // var webappUA = "uncomment this line and set your User Agent string here between these quotes, if you need to override it"
